744657a2dd28a88e22889d7062602c63f52ebb6c
Joshua <69637774+Joshyx@users.noreply.github.com>
2022-01-22 16:09:00 +0100

    Created solution for day7 part1

---
+++ day7/part1.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include <list>
+
+std::list<int> split(std::string line, std::string delimiter) {
+	std::list<int> values;
+	std::string token;
+	int start = 0;
+    int end = line.find(delimiter);
+    while (end != -1) {
+        values.push_back(std::stoi(line.substr(start, end - start)));
+        start = end + delimiter.size();
+        end = line.find(delimiter, start);
+    }
+    values.push_back(std::stoi(line.substr(start, end - start)));
+
+	return values;
+}
+
+std::list<int> getLines() {
+	std::string line;
+	std::ifstream file("input.txt");
+	getline(file, line);
+	file.close();
+
+	return split(line, ",");
+}
+
+int neededFuel(std::list<int> values, int average) {
+	int neededFuel = 0;
+	for(int value : values) {
+		neededFuel += abs(value - average);
+	}
+	return neededFuel;
+}
+
+int main() {
+	std::list<int> values = getLines();
+	values.sort();
+	std::list<int> fuelValues;
+
+	for(int i = values.front(); i <= values.back(); i++) {
+		fuelValues.push_back(neededFuel(values, i));
+	}
+	fuelValues.sort();
+	std::cout << fuelValues.front() << std::endl;
+};
